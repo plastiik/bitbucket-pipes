e08f4f52dacd36e6a89165bb91a3d75428a93bad
Joshua walker <josh.d.walker@me.com>
2020-11-17 13:48:05 +0000

    pipe test

--- aws-ecs-deploy-py/pipe/main.py
+++ aws-ecs-deploy-py/pipe/main.py
@@ -55,12 +55,10 @@ class ECSDeploy(Pipe):
 
     def get_client(self):
         try:
-            print(self.get_variable('AWS_PROFILE'))
-            boto3.session.Session(profile_name=self.get_variable('AWS_PROFILE'))
-            print(boto3.client('sts'))
-            
-            return boto3.session.Session(profile_name=self.get_variable('AWS_PROFILE')).client('ecs', region_name=self.get_variable('AWS_DEFAULT_REGION'))
-            # return boto3.client('ecs', region_name=self.get_variable('AWS_DEFAULT_REGION'))
+            # print(self.get_variable('AWS_PROFILE'))
+            # boto3.session.Session(profile_name=self.get_variable('AWS_PROFILE'))
+            # return boto3.session.Session(profile_name=self.get_variable('AWS_PROFILE')).client('ecs', region_name=self.get_variable('AWS_DEFAULT_REGION'))
+            return boto3.client('ecs', region_name=self.get_variable('AWS_DEFAULT_REGION'))
         except ClientError as err:
             self.fail("Failed to create boto3 client.\n" + str(err))
 
