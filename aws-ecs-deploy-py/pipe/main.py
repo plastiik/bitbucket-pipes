c0c157639d4b9fd91ff3626c048296c0ed51174f
Joshua walker <josh.d.walker@me.com>
2020-11-24 13:00:01 +0000

    remove get_profiles()

--- aws-ecs-deploy-py/pipe/main.py
+++ aws-ecs-deploy-py/pipe/main.py
@@ -58,7 +58,6 @@ class ECSDeploy(Pipe):
         try:
             # print(self.get_variable('AWS_PROFILE'))
             # boto3.session.Session(profile_name=self.get_variable('AWS_PROFILE'))
-            print get_profiles()
             return boto3.session.Session(profile_name=self.get_variable('AWS_PROFILE')).client('ecs', region_name=self.get_variable('AWS_DEFAULT_REGION'))
             # return boto3.client('ecs', region_name=self.get_variable('AWS_DEFAULT_REGION'))
         except ClientError as err:
